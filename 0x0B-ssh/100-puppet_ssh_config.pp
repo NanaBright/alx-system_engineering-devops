bda8d2f4fe9bd3e2a1c930739d9c1fd1d0ea3420
Bright <nanabryte.nb@gmail.com>
2023-08-07 19:04:46 -0700

    tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# puppet config
+
+file { 'etc/ssh/ssh_config':
+ensure => present.
+
+content =>"
+
+#SSH client configuration
+host"
+IdentifyFile ~/.ssh/school
+PasswordAuthentication no
+}
